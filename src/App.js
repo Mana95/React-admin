a20b1114d604f7f8956b4a4bd331fe5d9db30ab8
manoj <manojdharmarthne@gmail.com>
2024-10-13 18:20:04 +0530

    implement routing with authentication

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import NotFound from './views/NotFound';
-import ProductDetails from './components/ProductDetails';
-import { Suspense } from 'react';
-import Register from './components/Access/Register';
-import Login from './components/Access/Login';
-import CreateMembership from './views/Memberships/CreateMembership';
-import Membershiptable from './views/Memberships/Membershiptable';
-import Viewmembership from './views/Memberships/Viewmember';
-import NewSupplier from './views/administration/suppliers/NewSupplier';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Suspense } from "react";
+import AuthRoutes from "./routes/AuthRoutes";
+import UnAuthRoutes from "./routes/UnAuthRoutes";
 
-
-
-const App = ()=> {
-
+const App = () => {
   return (
     <>
-    <Suspense fallback={<div className="container">Loading...</div>}>
-    <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/new-membership" element={<CreateMembership />} />
-          <Route path="/membertable" element={<Membershiptable />} />
-          <Route path="/viewmember" element={<Viewmembership />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/products/:slug" element={<ProductDetails />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/new-supplier" element={<NewSupplier />} />
-    </Routes>
-    </Suspense>
+      <Suspense fallback={<div className="container">Loading...</div>}>
+        <Routes>
+          {AuthRoutes}
+          {UnAuthRoutes}
+        </Routes>
+      </Suspense>
     </>
   );
-}
+};
 
 export default App;
--- src/guards/UnAuthGuard.js
+++ src/guards/UnAuthGuard.js
@@ -1,11 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const UnAuthGuard = ({component}) => {
-    useEffect(() => {
-        console.log("UnAuth Guard");
-    }, [component]);
+const UnAuthGuard = ({ component }) => {
+  const navigate = useNavigate();
 
-    return <>{component}</>
-}
+  useEffect(() => {
+    // Assume this function checks if the user is logged in
+    const isAuthenticated = () => {
+      // Replace with actual authentication check
+      return !!localStorage.getItem("authToken");
+    };
+
+    if (isAuthenticated()) {
+      console.log("User already authenticated, redirecting to home");
+      navigate("/"); // Redirect to home page if already authenticated
+    }
+  }, [navigate]);
+
+  return <>{component}</>;
+};
 
 export default UnAuthGuard;
