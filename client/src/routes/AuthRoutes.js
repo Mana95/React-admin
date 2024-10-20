cff33efab69af876c845c3b25c7f4fbf557f49b6
manoj <manojdharmarthne@gmail.com>
2024-10-20 19:46:30 +0530

    Dashboard icon card implementation

--- client/src/routes/AuthRoutes.js
+++ client/src/routes/AuthRoutes.js
@@ -1,18 +1,14 @@
 import { Route } from "react-router-dom";
 import MemberShipTable from "../views/Memberships/Membershiptable";
-import Home from "../components/home/Home";
+import Home from "../views/home/Home";
 
 const AuthRoutes = [
-  // <Route
-  //   key="ProtectedHome"
-  //   path="/"
-  //   element={<AuthGuard component={<Home />} />}
-  // />,
   <Route
     key="Member-table"
     path="/member-table"
     element={<MemberShipTable />}
   />,
+  <Route key="" path="/" element={<Home />} />,
   <Route key="home" path="/home" element={<Home />} />,
 ];
 
